5edca92a4bf9f2e8d70496d708e6da6acae3a216
Puncoz Nepal <puncoz138@gmail.com>
2024-11-25 07:09:40 +0900

    build with rollup and tsup

---
+++ src/BikramSambat.ts
@@ -0,0 +1,7 @@
+class BikramSambat {
+  add(a: number, b: number) {
+    return a + b
+  }
+}
+
+export default BikramSambat
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import BikramSambat from "@/BikramSambat"
+
+export default BikramSambat
