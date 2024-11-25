4f099a166eca0df68242dd4caa0c596256208ed0
Puncoz Nepal <puncoz138@gmail.com>
2024-11-25 21:09:58 +0900

    lint staged check

--- src/BikramSambat.ts
+++ src/BikramSambat.ts
@@ -1,7 +1,15 @@
 class BikramSambat {
-    add(a: number, b: number): number {
-        return a + b
-    }
+  add(a: number, b: number): number {
+    return a + b
+  }
+
+  sub(a: number, b: number): number {
+    return a - b
+  }
+
+  mul(a: number, b: number): number {
+    return a * b
+  }
 }
 
 export default BikramSambat
