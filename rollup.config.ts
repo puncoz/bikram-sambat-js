3c991c3dcabfb85374df03b7bf63e67ae87158d0
Puncoz Nepal <puncoz138@gmail.com>
2024-11-27 04:04:30 +0900

    eslint rules updated and prettier added

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,19 @@
+import * as path from "node:path"
+
 import commonjs from "@rollup/plugin-commonjs"
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
-import * as path from "node:path"
 import { defineConfig } from "rollup"
 import { dts } from "rollup-plugin-dts"
 import { terser } from "rollup-plugin-terser"
+
 import pkg from "./package.json" with { type: "json" }
 
 // define the entry point
 const input = path.resolve(__dirname, "src/index.ts")
 
 // define external dependencies (dependencies that should not be included in the bundle)
-const external = [
-  ...Object.keys(pkg.devDependencies),
-]
+const external = [...Object.keys(pkg.devDependencies)]
 
 const config = defineConfig([
   {
